9a625bb153a30bc1a64dffad261338d6a829d2f2
atabiqa <atabiqasadlearn@gmail.com>
2024-02-09 08:31:57 +0800

    add trailing slash dan logger

--- routes/route.go
+++ routes/route.go
@@ -4,9 +4,12 @@ import (
 	"atabiqa/prakerja11/controllers/userr"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func InitRoute(e *echo.Echo) {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Logger())
 
 	e.GET("/users", userr.GetUserController)
 }
